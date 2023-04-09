8aba850218a24c2bf1fbd82429acb2643abdf613
imsohuy <nguyenhuuhuy203@gmail.com>
2023-04-09 16:58:12 +0700

    add filtered feature

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import bodyParser from 'body-parser'
+import express from 'express'
+
+import { deleteLocalFiles, filterImageFromURL } from './util/util'
 
 (async () => {
 
@@ -29,7 +30,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-  //! END @TODO1
+  app.get('/filteredimage', async (req, res) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url){
+      res.status(400).send('Image url is required');
+    }
+
+    const filtered_image = await filterImageFromURL(image_url);
+
+
+    res.status(200).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    });
+
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
